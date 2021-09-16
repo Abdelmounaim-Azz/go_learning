6f01feeacfc01d67ee7aca3369b28c912012cc50
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-16 23:33:47 +0100

    add go mod

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 12 {
-		t.Errorf("Excpectd length of 12,but got", len(d))
+		t.Errorf("Excpectd length of 12,but got %v", len(d))
 	}
 
 }
