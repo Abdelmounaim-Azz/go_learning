f0982f975cb4d8e6308a4d30e426a9fa5d83cf7b
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-16 23:57:04 +0100

    testing file IO

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -13,4 +16,15 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Three Of Clubs" {
 		t.Errorf("Excpectd last card of three of clubs,but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktest")
+	d := newDeck()
+	d.saveToFile("_decktest")
+	ldd := newDeckFromFile("_decktest")
+	if len(ldd) != 12 {
+		t.Errorf("Excpectd 12 cards,but got %v", len(ldd))
+	}
+	os.Remove("_decktest")
 }
