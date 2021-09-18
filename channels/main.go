7b49ab1473b56a5639aa489378e3f0b98cc45efb
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-18 03:32:41 +0100

    receiving one communication over channel and exiting!

--- channels/main.go
+++ channels/main.go
@@ -12,16 +12,20 @@ func main() {
 		"http://golang.org",
 		"http://stackoverflow.com",
 	}
+	c := make(chan string)
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is maybe down!")
+		c <- "Site is maybe down"
 		return
 	}
 	fmt.Println(link, "is up and running!")
+	c <- "Site is up and running!"
 }
