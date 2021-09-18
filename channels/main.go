ccf5aded10fc2f0b585e983fe0849b9e6b0c71e3
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-18 02:19:02 +0100

    channels intro status checker

---
+++ channels/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://www.prospecat.com",
+		"http://golang.org",
+		"http://stackoverflow.com",
+	}
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is maybe down!")
+		return
+	}
+	fmt.Println(link, "is up and running!")
+}
